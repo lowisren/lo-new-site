c04044f7796c047088702c18506adb5528d4b9ff
lowisren <lowisren@gmail.com>
2023-02-25 11:03:46 -0500

    new personal site

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div>
+            <h1 className="text-4xl">Who all gone be there?</h1>
+            <p>Some content</p>
+        </div>
+    );
+}
---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import page from './page'
+
+export const schemaTypes = [page, post, author, category, blockContent]
